Fix thresholding precision, recall

run_model_debug.py
@@ -9,11 +9,11 @@ from tensorpack.utils.gpu import get_num_gpu
 
 import tensornets as tn
 import sklearn
-from tensorlayer.cost import * #binary_cross_entropy, absolute_difference_error, dice_coe, cross_entropy
+# from tensorlayer.cost import * #binary_cross_entropy, absolute_difference_error, dice_coe, cross_entropy
 from chexpert import *
 """
 To train:
-    ./cifar10-resnet.py --gpu 0,1
+    
 """
 
 BATCH = 128
@@ -37,6 +37,7 @@ class AUCStatistics(object):
         self.corr_pos = 0   # correct predict positive
         self.corr_neg = 0   # correct predict negative
         self.auc = 0
+        self.shape = [-1, -1]
 
     def feed(self, pred, label):
         """
@@ -45,6 +46,9 @@ class AUCStatistics(object):
             label (np.ndarray): binary array of the same size.
         """
         assert pred.shape == label.shape, "{} != {}".format(pred.shape, label.shape)
+        pred = (pred > 0.5)*1 # Threshold
+        label = (label > 0.5)*1 # Threshold
+        self.shape = pred.shape
         self.nr_pos += (label == 1).sum()
         self.nr_neg += (label == 0).sum()
         self.nr_pred_pos += (pred == 1).sum()
@@ -85,7 +89,6 @@ class AUCStatistics(object):
     def roc(self):
         return self.auc
     
-
 class AUCStats(Inferencer):
     """
     Compute precision / recall in binary classification, given the
@@ -114,6 +117,8 @@ class AUCStats(Inferencer):
 
     def _after_inference(self):
         return {
+                # self.prefix + '_shape[0]': self.stat.shape[0],
+                # self.prefix + '_shape[1]': self.stat.shape[1],
                 self.prefix + '_nr_pos': self.stat.nr_pos,
                 self.prefix + '_nr_neg': self.stat.nr_neg,
                 self.prefix + '_nr_pred_pos': self.stat.nr_pred_pos,
@@ -125,7 +130,6 @@ class AUCStats(Inferencer):
                 self.prefix + '_roc': self.stat.roc
                 }
  
-
 def visualize_tensors(name, imgs, scale_func=lambda x: (x + 1.) * 128., max_outputs=1):
     """Generate tensor for TensorBoard (casting, clipping)
     Args:
@@ -140,6 +144,9 @@ def visualize_tensors(name, imgs, scale_func=lambda x: (x + 1.) * 128., max_outp
     xy = tf.cast(tf.clip_by_value(xy, 0, 255), tf.uint8, name='viz')
     tf.summary.image(name, xy, max_outputs=30)
 
+def BNLReLU(x, name=None):
+    x = BatchNorm('bn', x)
+    return tf.nn.leaky_relu(x, alpha=0.2, name=name)
 
 class Model(ModelDesc):
     def __init__(self, name='ResNet50', mode='small'):
@@ -177,47 +184,106 @@ class Model(ModelDesc):
     def inputs(self):
         if self.mode== 'space_to_depth':
             return [tf.TensorSpec([None, 3072, 3072, 1], tf.float32, 'image'),
-                    tf.TensorSpec([None, GROUP], tf.float32, 'label')]
+                    tf.TensorSpec([None, GROUP], tf.float32, 'label'), 
+                    tf.TensorSpec([None, 320, 320, 1], tf.float32, 'fname'),]
         else:
             return [tf.TensorSpec([None, SHAPE, SHAPE, 1], tf.float32, 'image'),
-                    tf.TensorSpec([None, GROUP], tf.float32, 'label')]
+                    tf.TensorSpec([None, GROUP], tf.float32, 'label'), 
+                    tf.TensorSpec([None, SHAPE, SHAPE, 1], tf.float32, 'fname'),
+                    ]
         
 
-    def build_graph(self, image, label):
+    def build_graph(self, image, label, fname):
         if self.mode== 'space_to_depth':
             image = tf.space_to_depth(image, SHAPE)
-
-        image = image / 255.0
+        image = tf.cast(image, tf.float32)
+        label = tf.cast(label, tf.float32)
+        fname = tf.cast(fname, tf.float32)
+        image = image / 128.0 - 1.0
+        fname = fname / 128.0 - 1.0
         assert tf.test.is_gpu_available()
         
         # with argscope([Conv2D, BatchNorm, GlobalAvgPooling], data_format='channels_last'), \
         #         argscope(Conv2D, kernel_initializer=tf.variance_scaling_initializer(scale=1.0 / 3, mode='fan_in', distribution='uniform')):
-        logit = self.net(image, stem=True, is_training=True)
-        logit = tf.reduce_mean(logit, [1, 2], name='avgpool')
-        # logit = FullyConnected('fc1', logit, 1024, activation='relu',
-        #                         kernel_initializer=tf.random_normal_initializer(stddev=1e-3))
-        # logit = FullyConnected('fc2', logit, 512, activation='relu',
-        #                         kernel_initializer=tf.random_normal_initializer(stddev=1e-3))
-        logit = FullyConnected('fc0', logit, GROUP, activation='sigmoid',
-                                kernel_initializer=tf.random_normal_initializer(stddev=1e-3))
-        logit = tf.identity(logit, name='logit')
+        with tf.variable_scope('Model'):
+            output = self.net(image, stem=True, is_training=True)
+            # if self.name=='ResNeXt50c32':
+            #     output = tn.ResNeXt50c32(image, stem=True, classes=GROUP, is_training=True)
+            # output = tf.reduce_mean(output, [1, 2], name='avgpool')
+            # output.print_outputs()
+            output = GlobalAvgPooling('gap', output)
+            output = FullyConnected('fc1', output, 1024, activation=BNLReLU,
+                                    kernel_initializer=tf.random_normal_initializer(stddev=1e-3))
+            output = FullyConnected('fc', output, 512, activation=BNLReLU,
+                                    kernel_initializer=tf.random_normal_initializer(stddev=1e-3))
+        linear = FullyConnected('linear', output, GROUP, activation=BNLReLU,
+                                    kernel_initializer=tf.random_normal_initializer(stddev=1e-3))
 
-        loss = tf.identity(1.0 - dice_coe(logit, label, axis=[0,1], loss_type='jaccard'), 
-                                 name='loss')  
-        # auc = roc_auc_score(logit, label)
+        loss_xentropy = tf.losses.sigmoid_cross_entropy(label, linear, reduction=tf.losses.Reduction.NONE)
+        # loss_xentropy = tf.identity(loss_xentropy, name='loss_xentropy')
+        loss_xentropy = tf.reduce_mean(loss_xentropy, name='loss_xentropy')
 
-        # weight decay on all W of fc layers
+        logit = tf.sigmoid(linear, name='logit')
+        def dice_coe(output, target, loss_type='jaccard', axis=(1, 2, 3), smooth=1e-5):
+            """Soft dice (Sørensen or Jaccard) coefficient for comparing the similarity
+            of two batch of data, usually be used for binary image segmentation
+            i.e. labels are binary. The coefficient between 0 to 1, 1 means totally match.
+
+            Parameters
+            -----------
+            output : Tensor
+                A distribution with shape: [batch_size, ....], (any dimensions).
+            target : Tensor
+                The target distribution, format the same with `output`.
+            loss_type : str
+                ``jaccard`` or ``sorensen``, default is ``jaccard``.
+            axis : tuple of int
+                All dimensions are reduced, default ``[1,2,3]``.
+            smooth : float
+                This small value will be added to the numerator and denominator.
+                    - If both output and target are empty, it makes sure dice is 1.
+                    - If either output or target are empty (all pixels are background), dice = ```smooth/(small_value + smooth)``, then if smooth is very small, dice close to 0 (even the image values lower than the threshold), so in this case, higher smooth can have a higher dice.
+
+            Examples
+            ---------
+            >>> import tensorlayer as tl
+            >>> outputs = tl.act.pixel_wise_softmax(outputs)
+            >>> dice_loss = 1 - tl.cost.dice_coe(outputs, y_)
+
+            References
+            -----------
+            - `Wiki-Dice <https://en.wikipedia.org/wiki/Sørensen–Dice_coefficient>`__
+
+            """
+            inse = tf.reduce_sum(output * target, axis=axis)
+            if loss_type == 'jaccard':
+                l = tf.reduce_sum(output * output, axis=axis)
+                r = tf.reduce_sum(target * target, axis=axis)
+            elif loss_type == 'sorensen':
+                l = tf.reduce_sum(output, axis=axis)
+                r = tf.reduce_sum(target, axis=axis)
+            else:
+                raise Exception("Unknow loss_type")
+            
+            dice = (2. * inse + smooth) / (l + r + smooth) # 1 minus
+            ##
+            dice = 1 - tf.reduce_mean(dice, name='dice_coe')
+            return dice
+
+        loss_dice = tf.identity(dice_coe(logit, label, axis=(1), loss_type='jaccard'), name='loss_dice') 
+        
+        # # weight decay on all W of fc layers
         wd_w = tf.train.exponential_decay(0.0002, get_global_step_var(),
                                           500000, 0.2, True)
         wd_loss = tf.multiply(wd_w, regularize_cost('.*/W', tf.nn.l2_loss), name='wd_loss') 
-        # add_moving_summary(auc)
-        add_moving_summary(loss)
+        add_moving_summary(loss_xentropy)
+        add_moving_summary(loss_dice)
         add_moving_summary(wd_loss)
 
         # Visualization
         add_param_summary(('.*/W', ['histogram']))   # monitor W
-        visualize_tensors('image', [image], max_outputs=max(64, BATCH))
-        cost = tf.add_n([loss, wd_loss], name='cost')
+        visualize_tensors('image', [image, fname], max_outputs=max(64, BATCH))
+        cost = tf.add_n([loss_xentropy, loss_dice, wd_loss], name='cost')
         return cost
 
     def optimizer(self):
@@ -305,7 +371,7 @@ if __name__ == '__main__':
     parser.add_argument('--mode', help='small | random_patch | space_to_depth', default='small')
     parser.add_argument('--debug', help='Small size', action='store_true')
     parser.add_argument('--model', help='Model name', default='ResNet50')
-    parser.add_argument('--group', type=int, default=14)
+    parser.add_argument('--group', type=int, default=5)
     parser.add_argument('--batch', type=int, default=32)
     parser.add_argument('--shape', type=int, default=320)
     parser.add_argument('--train', action='store_true', help='train')
@@ -331,14 +397,13 @@ if __name__ == '__main__':
     ds_valid.reset_state()
 
     ds_train = AugmentImageComponent(ds_train, ag_train, 0)
-    ds_valid = AugmentImageComponent(ds_valid, ag_valid, 0)
-    
+    ds_valid = AugmentImageComponent(ds_valid, ag_valid, 0) 
+
     ds_train = BatchData(ds_train, BATCH)
     ds_train = MultiProcessRunnerZMQ(ds_train, num_proc=8)
     
     ds_valid = BatchData(ds_valid, BATCH)
-    ds_valid = MultiProcessRunnerZMQ(ds_valid, num_proc=1)
-    
+    # ds_valid = MultiProcessRunnerZMQ(ds_valid, num_proc=1)
     ds_train = PrintData(ds_train)
     ds_valid = PrintData(ds_valid)
     
@@ -349,11 +414,13 @@ if __name__ == '__main__':
         dataflow=ds_train,
         callbacks=[
             PeriodicTrigger(ModelSaver(), every_k_epochs=10),
+            # InferenceRunner(ds_train,
+            #                 [AUCStats('logit', 'label', prefix='train'),]),
             InferenceRunner(ds_valid,
-                            [   AUCStats('logit', 'label', prefix='valid'),
-                             ]),
+                            [ScalarStats('loss_dice'), ScalarStats('loss_xentropy'),
+                            AUCStats('logit', 'label', prefix='valid'),]),
             ScheduledHyperParamSetter('learning_rate',
-                                      [(0, 0.1), (50, 0.01), (100, 0.001), (150, 0.0001)])
+                                      [(0, 1e-2), (50, 1e-3), (100, 1e-4), (150, 1e-5)])
         ],
         max_epoch=200,
         session_init=SmartInit(args.load),